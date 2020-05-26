6b8d091ce2a99c8e91917112bad930ea9a8a92a3
oggyman <oggyla@gmail.com>
2020-05-26 14:42:18 -0700

    changed some routes

--- server/routes/myKanban.routes.js
+++ server/routes/myKanban.routes.js
@@ -3,6 +3,6 @@ const KanbanController = require("../controllers/myKanban.controller");
 module.exports = app => {
     app.get("/api/projects/all", KanbanController.getAllProjects);
     app.post("/api/projects/new", KanbanController.createNewProject);
-    app.put("/api/projects/update/:id", KanbanController.updateExistingProject);
+    app.put("/api/projects/update/:id", KanbanController.updateExistingProjectStatus);
     app.delete("/api/projects/delete/:id", KanbanController.deleteAnExistingProject);
 };
