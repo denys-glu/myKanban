ce7ec30f905e82e329b2dfba18d22e589055e3ee
oggyman <oggyla@gmail.com>
2020-05-29 15:04:01 -0700

    added build folder

---
+++ client/build/precache-manifest.f60614e197ab2b79005826180cc5e23e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e373635608d80ca63e50156446c484c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5355b1d5c0328dbfcb60",
+    "url": "/static/css/main.8fdf68fb.chunk.css"
+  },
+  {
+    "revision": "4822183804a0b12495a9",
+    "url": "/static/js/2.99b2947d.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.99b2947d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5355b1d5c0328dbfcb60",
+    "url": "/static/js/main.600e8fba.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "301a37db21a946393ceef7c0f7be3855",
+    "url": "/static/media/bueNote.301a37db.png"
+  },
+  {
+    "revision": "c65d77b8e650ea68951410227b38726a",
+    "url": "/static/media/corkBoardCropped.c65d77b8.png"
+  },
+  {
+    "revision": "92e3ff5e45baf8643047fb99638cc4f3",
+    "url": "/static/media/greenNote.92e3ff5e.png"
+  },
+  {
+    "revision": "bb1b5e4c86baa5a2789ae536c1e6324b",
+    "url": "/static/media/yellowNote.bb1b5e4c.png"
+  }
+]);
