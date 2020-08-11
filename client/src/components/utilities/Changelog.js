4d99797bed6b5a8dc3e14df0eb2e52734a2b6d72
denys_gl <oggyla@gmail.com>
2020-08-11 11:06:12 -0700

    updated changelog

--- client/src/components/utilities/Changelog.js
+++ client/src/components/utilities/Changelog.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Link } from '@reach/router';
 
+import Storage from './Storage';
+
 const Changelog = () => {
+    const currProjectName =  Storage.get("currentSession")["name"];
     return (
         <div>
             <div className="container transparent-background p-3 mt-5 changelog">
                 <div className="row">
                     <div className="col text-right">
-                        <Link to="/"><h3>Go back to Dashboard</h3></Link>
+                        <Link to={`/project/${currProjectName}/tickets`}><h3>Go back to Dashboard</h3></Link>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col text-left">
                         <ul className="fs28">
+                            <li>8/11/2020
+                                <ul>
+                                    <li>Added Projects feature</li>
+                                    <li>Now you can create Projects and add tickets to them, and they will be in the right scope.</li>
+                                </ul>    
+                            </li>
                             <li>8/04/2020
                                 <ul>
                                     <li>Added API to create Projects</li>
