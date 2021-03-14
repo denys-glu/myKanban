3f046bb29db9a8ef8ec59e61321b97c17f4ef399
denys_glu <denis.glukhovskyy@gmail.com>
2021-03-14 16:33:58 -0700

    added env variable for db password

--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 const dbName = "MyKanban";
+const dbPassword = process.env.DB_PASSWORD
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
-mongoose.connect(`mongodb://localhost/${dbName}`, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
-	.then(() => console.log(`Established a connection to the database ${dbName}`))
-	.catch(err => console.log("Something went wrong when connecting to the database", err));
+mongoose.connect(`mongodb+srv://busabro:${dbPassword}@myshinycluster.qqcs1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log(`Established a connection to the database ${dbName}`))
+    .catch(err => console.log("Something went wrong when connecting to the database", err));
